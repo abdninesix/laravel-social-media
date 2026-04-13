215f384a63a658f53a277c24942398864c1a3e33
abdninesix <mohabd1995@gmail.com>
2026-04-13 17:04:57 +0500

    updated

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/posts', function () {
+    $posts = Post::all();
+    return response()->json($posts);
+});
+
+Route::get('/create', function () {
+    $post = Post::create([
+        'caption' => 'Hello World',
+    ]);
+
+    return response()->json($post);
 });
