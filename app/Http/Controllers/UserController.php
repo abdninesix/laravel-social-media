abe841ecb4825fdaf672b05eaf33fed01bac3251
abdninesix <mohabd1995@gmail.com>
2026-04-16 14:53:17 +0500

    updated

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,11 +7,25 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-     public function __construct(protected UserService $userService)
+    public function __construct(protected UserService $userService)
     {
     }
 
-    public function register() {}
-    
-    public function login() {}
+    public function register(Request $request)
+    {
+        $validatedData = $request->validate([
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|string'
+        ]);
+
+        if ($this->userService->register(email: $validatedData['email'], password: $validatedData['password'])) {
+            return response()->json(["error" => "Account already exists"], status:400);
+        }
+
+        return response();
+    }
+
+    public function login(Request $request)
+    {
+    }
 }
--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -8,5 +8,26 @@ use Illuminate\Support\Facades\Hash;
 
 class UserService
 {
-    
+    public function register(string $email, string $password): bool
+    {
+        $normalizedEmail = strtolower($email);
+        $user = User::query()
+            ->where('email', $normalizedEmail)
+            ->first();
+        if ($user != null) {
+            return false;
+        }
+        User::create([
+            'email' => $normalizedEmail,
+            'name' => "Abdullah",
+            'password' => Hash::make($password),
+            'description' => "A cool new user",
+        ]);
+        return true;
+    }
+
+    public function login(string $email, string $password)
+    {
+    }
+
 }
