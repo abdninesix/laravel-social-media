4dbf467e8c6bee70a830bab5dda7d23d5a1d5185
abdninesix <mohabd1995@gmail.com>
2026-04-18 12:40:45 +0500

    updated

--- app/Http/Controllers/GoogleAuthController.php
+++ app/Http/Controllers/GoogleAuthController.php
@@ -3,16 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Laravel\Socialite\Facades\Socialite;
 
 class GoogleAuthController extends Controller
 {
     public function redirect()
     {
-
+        return Socialite::driver('google')->redirect();
     }
 
     public function callback()
     {
-
+        $user = Socialite::driver('google')->user();
+        var_dump($user);
     }
 }
