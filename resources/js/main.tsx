b470a25407426c47d257efd7ca65c65d8d56a3c9
abdninesix <mohabd1995@gmail.com>
2026-04-27 11:34:25 +0500

    Updated

---
+++ resources/js/main.tsx
@@ -0,0 +1,10 @@
+import ReactDOM from "react-dom/client";
+
+const rootElement = document.getElementById("root")!;
+const root = ReactDOM.createRoot(rootElement);
+
+
+root.render(
+    <>
+    </>
+);
