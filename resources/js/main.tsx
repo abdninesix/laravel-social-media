768b405e3bc1060392945409dbdace74b6376303
abdninesix <mohabd1995@gmail.com>
2026-04-28 11:28:17 +0500

    Updated

--- resources/js/main.tsx
+++ resources/js/main.tsx
@@ -1,10 +1,10 @@
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import Application from "./Application";
 
 const rootElement = document.getElementById("root")!;
 const root = ReactDOM.createRoot(rootElement);
 
 
 root.render(
-    <App />
+    <Application />
 );
