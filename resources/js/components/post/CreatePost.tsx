cf534f3c12c4403fde2940d7644dff2053538a19
abdninesix <mohabd1995@gmail.com>
2026-04-30 16:46:48 +0500

    Updated

--- resources/js/components/post/CreatePost.tsx
+++ resources/js/components/post/CreatePost.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { PostsAPI } from "../../services/posts";
+import { GoImage } from "react-icons/go";
 
 const CreatePost = () => {
     const [caption, setCaption] = useState("");
@@ -27,32 +28,36 @@ const CreatePost = () => {
     };
 
     return (
-        <form onSubmit={handleSubmit} className="space-y-4">
-            <div>
-                <textarea
-                    value={caption}
-                    onChange={(e) => setCaption(e.target.value)}
-                    placeholder="What's on your mind?"
-                    className="border p-2 w-full"
-                />
+        <form onSubmit={handleSubmit} className="flex gap-4">
+
+            <textarea
+                value={caption}
+                onChange={(e) => setCaption(e.target.value)}
+                placeholder="What's on your mind?"
+                className="border p-2 w-full"
+                required
+            />
+
+            <input
+                type="file"
+                onChange={(e) =>
+                    setImage(e.target.files ? e.target.files[0] : null)
+                }
+                id="imageUpload"
+                className="hidden"
+            />
+
+            <div className="space-y-2">
+                <label htmlFor="imageUpload" className="text-blue-400 cursor-pointer"> <GoImage size={25} /></label>
+                <button
+                    type="submit"
+                    disabled={loading}
+                    className="h-fit bg-blue-500 text-white px-4 py-2 cursor-pointer"
+                >
+                    {loading ? "Posting..." : "Post"}
+                </button>
             </div>
 
-            <div>
-                <input
-                    type="file"
-                    onChange={(e) =>
-                        setImage(e.target.files ? e.target.files[0] : null)
-                    }
-                />
-            </div>
-
-            <button
-                type="submit"
-                disabled={loading}
-                className="bg-blue-500 text-white px-4 py-2"
-            >
-                {loading ? "Posting..." : "Create Post"}
-            </button>
         </form>
     );
 };
